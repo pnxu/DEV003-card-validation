eafcbec8009c63cf7f483bfbc16a51642a18c850
francisca <fcaavv@gmail.com>
2022-12-14 10:36:14 -0300

    ajustes visuales y cambios en el input tarjeta

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const submitButton = document.getElementById("submit-button");
 const creditCard = document.getElementById("card-number");
 const numbersBox = document.getElementById("mask");
 
+creditCard.addEventListener("input", function (e) {
+  e.target.value = e.target.value
+    .replace(/[^\dA-Z]/g, "")
+    .replace(/(.{4})/g, "$1 ")
+    .trim();
+});
+
 creditCard.addEventListener("keydown", () => {
   const maskedNumbers = validator.maskify(creditCard.value);
   numbersBox.innerHTML = maskedNumbers;
@@ -20,12 +27,13 @@ submitButton.addEventListener("click", (event) => {
   const year = Number.parseInt(
     document.getElementById("expiration-year").value
   );
-  if (creditCard.value.length !== 16) return alert("Ingresa 16 caracteres");
-  if (customerName.length === 0) return alert("Nombre de cliente inválido");
+  if (creditCard.value.length !== 19) return alert("Ingresa 16 caracteres");
+  if (customerName.length === 0) return alert("Ingresa Nombre Cliente");
   if (month < 0 && month > 13) return alert("Mes invalido");
   if (year < 2020 && year > 2040) return alert("Año invalido");
-  if (cvv.length !== 3) return alert("cvv invalido");
+  if (cvv.length !== 3) return alert("CVV invalido");
 
+  //isValid function
   const validatorResponse = validator.isValid(creditCard.value);
   if (!validatorResponse) return alert("Tarjeta de credito inválida");
   // console.log({ validatorResponse, creditCard, customerName, year, month });
@@ -33,4 +41,12 @@ submitButton.addEventListener("click", (event) => {
     window.location = "./success.html";
   }, 2000);
   alert("Datos guardados exitosamente, redirigiendo...");
+});
+
+// if (event.key === ) creditCard = creditCard.value.slice(0, -1);
+creditCard.addEventListener("keypress", (event) => {
+  const ch = String.fromCharCode;
+  if (!/[0-9]/.test(ch)) creditCard = creditCard.value.slice(0, -1);
+  event.preventDefault();
+  event.stopPropagation();
 });
