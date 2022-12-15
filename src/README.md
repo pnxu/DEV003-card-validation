c5472b361df91e9a9d6676280434eec4d84e16a9
francisca <fcaavv@gmail.com>
2022-12-15 03:03:40 -0300

    ultimos cambios y README

---
+++ src/README.md
@@ -0,0 +1,9 @@
+# Tarjeta de crédito válida
+
+En este proyecto contruí una aplicación web que permita al usuario validar una tarjeta de crédito e implementar funcionalidad para ocultar todos los dígitos menos los últimos cuatro.
+
+Mi proyecto lo hice basado en una Wishlist o lista de deseos en donde al usuario se le permita comprar un producto para regalar a un destinatario e ingrese sus datos de compra.
+
+Lo hice pensando en las usuarias con contenido visual agradable y simple.
+
+Prototipo incial :
--- src/index.js
+++ src/index.js
@@ -5,13 +5,6 @@ const submitButton = document.getElementById("submit-button");
 const creditCard = document.getElementById("card-number");
 const numbersBox = document.getElementById("mask");
 
-creditCard.addEventListener("input", function (e) {
-  e.target.value = e.target.value
-    .replace(/[^\dA-Z]/g, "")
-    .replace(/(.{4})/g, "$1 ")
-    .trim();
-});
-
 creditCard.addEventListener("keydown", () => {
   const maskedNumbers = validator.maskify(creditCard.value);
   numbersBox.innerHTML = maskedNumbers;
@@ -27,10 +20,10 @@ submitButton.addEventListener("click", (event) => {
   const year = Number.parseInt(
     document.getElementById("expiration-year").value
   );
-  if (creditCard.value.length !== 19) return alert("Ingresa 16 caracteres");
+  if (creditCard.value.length !== 16) return alert("Ingresa 16 caracteres");
   if (customerName.length === 0) return alert("Ingresa Nombre Cliente");
-  if (month < 0 && month > 13) return alert("Mes invalido");
-  if (year < 2020 && year > 2040) return alert("Año invalido");
+  if (month < 0 || month > 13) return alert("Mes invalido");
+  if (year < 2020 || year > 2030) return alert("Año invalido");
   if (cvv.length !== 3) return alert("CVV invalido");
 
   //isValid function
@@ -41,12 +34,4 @@ submitButton.addEventListener("click", (event) => {
     window.location = "./success.html";
   }, 2000);
   alert("Datos guardados exitosamente, redirigiendo...");
-});
-
-// if (event.key === ) creditCard = creditCard.value.slice(0, -1);
-creditCard.addEventListener("keypress", (event) => {
-  const ch = String.fromCharCode;
-  if (!/[0-9]/.test(ch)) creditCard = creditCard.value.slice(0, -1);
-  event.preventDefault();
-  event.stopPropagation();
 });
